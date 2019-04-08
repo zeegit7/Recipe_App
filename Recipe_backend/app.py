56663d63462b37a2e529babb56287ecb20033dbb
Zeeshan Ali <zeeshanalidba@gmail.com>
2019-04-08 02:06:09 -0700

    included error checking

--- Recipe_backend/app.py
+++ Recipe_backend/app.py
@@ -27,41 +27,47 @@ mysql = MySQL(app)
 
 @app.route("/", methods = ['GET'])
 def get_recipes():
-    cursor = mysql.get_db().cursor()
-    result = cursor.execute("SELECT * from recipes;")
-    if result > 0:
-        recipes = cursor.fetchall()
-        all_recipes = []
-        for x in range(0,len(recipes)):
-            new_recipe={
-                'recipe_name' : recipes[x][0],
-                'ingredients' : recipes[x][1],
-                'instructions': recipes[x][2],
-                'serving_size': recipes[x][3],
-                'category' : recipes[x][4],
-                'notes' : recipes[x][5],
-                'date_added' : recipes[x][6],
-                'date_modified' : recipes[x][7]
-            }
-            all_recipes.append(new_recipe)
-            cursor.close()
-        return jsonify(all_recipes),201
-    return ('No data!'),400
+    try:
+        cursor = mysql.get_db().cursor()
+        result = cursor.execute("SELECT * from recipes;")
+        if result > 0:
+            recipes = cursor.fetchall()
+            all_recipes = []
+            for x in range(0,len(recipes)):
+                new_recipe={
+                    'recipe_name' : recipes[x][0],
+                    'ingredients' : recipes[x][1],
+                    'instructions': recipes[x][2],
+                    'serving_size': recipes[x][3],
+                    'category' : recipes[x][4],
+                    'notes' : recipes[x][5],
+                    'date_added' : recipes[x][6],
+                    'date_modified' : recipes[x][7]
+                }
+                all_recipes.append(new_recipe)
+                cursor.close()
+            return jsonify(all_recipes),201
+    except Exception as err:
+        print(str(err))
+        return ('No data!'),400
 
 
 @app.route("/", methods=["DELETE"])
 def delete_recipe():
-    req_data = request.get_json()
-    recipe_name = req_data['recipe_name']
-    print(recipe_name)
-    conn = mysql.get_db()
-    cursor = conn.cursor()
-    del_query = "DELETE FROM recipes WHERE recipe_name LIKE %s"
-    if(cursor.execute(del_query, recipe_name)):
-        conn.commit()
-        cursor.close()
-        return jsonify('Deleted the  recipe'),204
-    return jsonify('Deleted failed'),400
+    try:
+        req_data = request.get_json()
+        recipe_name = req_data['recipe_name']
+        print(recipe_name)
+        conn = mysql.get_db()
+        cursor = conn.cursor()
+        del_query = "DELETE FROM recipes WHERE recipe_name LIKE %s"
+        if(cursor.execute(del_query, recipe_name)):
+            conn.commit()
+            cursor.close()
+            return jsonify('Deleted the  recipe'),204
+    except Exception as err:
+        print(str(err))
+        return jsonify('Deleted failed'),400
 
 
 
@@ -75,39 +81,45 @@ def add_recipe():
     category = req_data['category']
     notes = req_data['notes']
     now = datetime.datetime.now()
+
+
+
     print(now)
     print(req_data)
-    conn = mysql.get_db()
-    cursor = conn.cursor()
-    if request.method == "POST":
-        cursor.execute(
-            """INSERT INTO
-                recipes (
-                    recipe_name, 
-                    ingredients, 
-                    instructions, 
-                    serving_size, 
-                    category, 
-                    notes,
-                    date_added
-                    )
-            VALUES (%s, %s, %s, %s, %s, %s, %s)""", (recipe_name,ingredients,instructions,int(serving_size),category,notes,now))
-    elif request.method == "PUT":
-        cursor.execute(
-            """UPDATE 
-                recipes SET
-                    ingredients = %s, 
-                    instructions = %s, 
-                    serving_size = %s, 
-                    category = %s, 
-                    notes= %s,
-                    date_modified = %s
-                    WHERE recipe_name = %s
-                        """, (ingredients,instructions,int(serving_size),category,notes,now,recipe_name))
-    conn.commit()
-    cursor.close()
-    return jsonify('Added/Updated the recipe'),204
-    #return jsonify('Add failed'),400
+    try:
+        conn = mysql.get_db()
+        cursor = conn.cursor()
+        if request.method == "POST":
+            cursor.execute(
+                """INSERT INTO
+                    recipes (
+                        recipe_name, 
+                        ingredients, 
+                        instructions, 
+                        serving_size, 
+                        category, 
+                        notes,
+                        date_added
+                        )
+                VALUES (%s, %s, %s, %s, %s, %s, %s)""", (recipe_name,ingredients,instructions,int(serving_size),category,notes,now))
+        elif request.method == "PUT":
+            cursor.execute(
+                """UPDATE 
+                    recipes SET
+                        ingredients = %s, 
+                        instructions = %s, 
+                        serving_size = %s, 
+                        category = %s, 
+                        notes= %s,
+                        date_modified = %s
+                        WHERE recipe_name = %s
+                            """, (ingredients,instructions,int(serving_size),category,notes,now,recipe_name))
+        conn.commit()
+        cursor.close()
+        return jsonify('Added/Updated the recipe'),204
+    except Exception as err:
+        print(str(err))
+        return jsonify('Add/Update recipe failed'),400
     
 
     
